ac846597a629970ebd826a463b60686c288aab0d
Kevin Anderson <kevinanderso@gmail.com>
2013-07-13 23:11:05 -0600

    updated character set

--- util.c
+++ util.c
@@ -51,8 +51,8 @@ int strToInt(char* s){
 	return n;
 }
 
-char table[2][41] = {{0x0B, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C, 0x39, 0x1C, 0x34, 0x33, 0x35},
-					 {'0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z' , ' ' , '\n', '.' , ',' , '/'}};
+char table[2][49] = {{0x0B, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C, 0x39, 0x1C, 0x34, 0x33, 0x35, 0x0D, 0x0C, 0x29, 0x1A, 0x1B, 0x2B, 0x28, 0x27},
+					 {'0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z' , ' ' , '\n', '.' , ',' , '/', '=' , '-' , '`' , '[' , ']' , '\\', '\'', ';' }};
 
 char scancodeToAscii(unsigned char scan, int shift, int ctrl, int caps){
 	int i;
@@ -73,6 +73,14 @@ char scancodeToAscii(unsigned char scan, int shift, int ctrl, int caps){
 				if(table[1][i] == ',') return '<';
 				if(table[1][i] == '.') return '>';
 				if(table[1][i] == '/') return '?';
+				if(table[1][i] == '=') return '+';
+				if(table[1][i] == '-') return '_';
+				if(table[1][i] == '`') return '~';
+				if(table[1][i] == '[') return '{';
+				if(table[1][i] == ']') return '}';
+				if(table[1][i] == '\\') return '|';
+				if(table[1][i] == '\'') return '"';
+				if(table[1][i] == ';') return ':';
 			}
 			return table[1][i];
 		}
